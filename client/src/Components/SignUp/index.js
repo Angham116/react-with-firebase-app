54c379f2d475cb7d5cc225cb654889a3d382aa83
Angham116 <angham.1161995@hotmail.com>
2019-07-15 15:59:36 +0300

    feat: make sign up form

--- client/src/Components/SignUp/index.js
+++ client/src/Components/SignUp/index.js
@@ -1,9 +1,101 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { Form, Input, Icon, Checkbox, Button} from 'antd';
 
 export default class SignUp extends Component{
-  state = {};
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    confirmedPass: '',
+    error: null,
+  };
+
+  handleOnChange = (event) => this.setState({ [event.target.name] : event.target.value });
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  }
 
   render(){
-    return <h1> SignUp </h1>
+    const { username, email, password, confirmedPass, error } = this.state;
+
+    const isInvalid = 
+      password !== confirmedPass ||
+      password === '' ||
+      username === '' ||
+      email === '';
+
+    return (
+      <>
+        <h1>Sign up</h1>
+        <Form onSubmit={this.handleSubmit} className="login-form">
+          {error ? <span className="err__msg">{error.message}</span> : null}
+          <Form.Item>
+            <label>
+              Username:
+              <Input
+                prefix={<Icon type="user"style={{ color: 'rgba(0,0,0,.25)' }} />}
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={this.handleOnChange}
+              />
+            </label>
+          </Form.Item>
+          <Form.Item>
+            <label>
+              E-mail:
+              <Input
+                name="email"
+                type="text"
+                placeholder="e.g: someone.hotmail.com"
+                onChange={this.handleOnChange}
+              />
+            </label>
+          </Form.Item>
+          <Form.Item>
+            <label>
+              Password:
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={this.handleOnChange}
+              />
+            </label>
+          </Form.Item>
+          <Form.Item>
+            <label>
+              Confirm Password:
+              <Input
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                name="confirmedPass"
+                type="password"
+                placeholder="Password"
+                onChange={this.handleOnChange}
+              />
+            </label>
+          </Form.Item>
+          <Form.Item>
+            <>
+              <Checkbox>Remember me</Checkbox>
+              <Button 
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                disabled={isInvalid}
+              >
+                Sign up
+              </Button>
+              Or Already have an account! <Link to="/login"> Login </Link>
+            </>
+          </Form.Item>
+        </Form>
+    </>
+    )
   }
 }
